da830b6da082e587de3316c4c3d347f75e7e7f79
carlen32 <108589578+carlen32@users.noreply.github.com>
2022-12-06 17:42:25 -0700

    nothing important

--- src/controller/catalogController.ts
+++ src/controller/catalogController.ts
@@ -130,6 +130,7 @@ const getCatalogItems = (req: Request, res: Response) => {
   const check = Object.keys(req.body).filter(
     (x) => x === "designStyle" || x == "usePackages"
   );
+  console.log(check)
   const workArray = Object.fromEntries(check.map((x) => [x, req.body[x]]));
   CatalogItem.find()
     .then((items) => {
